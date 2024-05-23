79a9352b915e9051d2b2164d0d06bd5f1f99b944
morsema21 <167927235+morsema21@users.noreply.github.com>
2024-05-23 12:59:43 -0500

    Add files via upload

---
+++ vault.js
@@ -0,0 +1,8 @@
+alert("welcome");
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+);
+let num1 = 2 * 5;
+let num2 = num1 + 30;
+let num3 = num2 - 1;
+alert(num1 + " - " + num2 + " - " + num3);
